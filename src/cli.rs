2decab394a3b59ccf37d769303c13fe2cb7eaf95
FrostPrice <barbosamat16@gmail.com>
2022-06-14 13:30:04 -0300

    Feat: Add a cli info

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,22 @@
+use std::env;
+
 pub fn run() {
-    // let args: Vec<String> =
+    let args: Vec<String> = env::args().collect();
+    let command = args[1].clone();
+    let name = "Brad";
+    let status = "100%";
+
+    // println!("Args: {:?}", args);
+    // println!("Command: {:?}", command);
+
+    if command == "hello" {
+        println!("Hi {}, how are you?", name);
+    } else if command == "status" {
+        println!("Status is {}", status);
+    } else {
+        println!("That is not a valid command")
+    }
 }
+
+// std::env::args is able to get arguments when passed during the execution of cargo run
+// The args.collect() will return a Vector, where the first element is always the target of the executible
